a31559407332b98798606fdc138a7359c45cfda3
Albert Mesa <andres890625@gmail.com>
2022-09-06 21:15:22 -0500

    Stupid coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,11 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params["question"]
+   # if @question.include? ()
+   #@answer =
+   #end
+  end
+end
